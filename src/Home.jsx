103a2c11b8573454db2462c8c31ad024a5ad9afd
KenjayevObid <obidkenjaev2192@gmail.com>
2024-02-13 18:37:37 +0500

    navbar color change

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,10 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <span style={{width: '400px', height: '400px', backgroundColor: 'crimson'}}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit amet
-        incidunt porro itaque eius facere eaque vel, debitis aperiam! Aut ab,
-        nostrum officia molestias, 
+      <span style={{width: '400px', height: '400px', backgroundColor: 'crimson', textAlign: 'center'}}>
+       Assalomu Alaykum
       </span>
     </div>
   );
